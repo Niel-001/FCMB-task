c9e458093c3d8b723d3dd90420b5958e874f5928
Niel-05 <shuggzydell97@gmail.com>
2024-12-05 16:18:11 +0100

    init

---
+++ sw.js
@@ -0,0 +1,7 @@
+var config = {
+    apiKey: 'AIzaSyDvXvz7yRJfqv-58YduIFP1chEkpdvm7J4',
+    messagingSenderId: '814948634122',
+    user_key: 'ADGMOT35CHFLVDHBJNIG50K9688EI8S5U85LJIDF7V3HT18UBR00',
+    siteid: '09f3b1f918b1225457321130a4845c42'
+};
+importScripts('//tw.netcore.co.in/sw.js');
